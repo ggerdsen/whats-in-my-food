ddbcca4006f99e1d13664e1358fe014f3dcd37d6
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-19 09:44:28 -0600

    Test finished

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Index" do
+  describe "As an user" do
+    it "I can search for food and get back details" do
+      visit "/"
+      expect(page).to have_button("Search")
+      fill_in :q, with: "sweet potatoes"
+      click_on "Search"
+      #first result
+      expect(current_path).to eq("/foods")
+      expect(page).to have_content("Description: SWEET POTATOES")
+      expect(page).to have_content("GTIN/UPC code: 070560951975")
+      expect(page).to have_content("Brand Owner: The Pictsweet Company")
+      expect(page).to have_content("Ingredients: SWEET POTATOES.")
+      #last result
+      expect(current_path).to eq("/foods")
+      expect(page).to have_content("Description: SWEET POTATOES")
+      expect(page).to have_content("GTIN/UPC code: 095829400193")
+      expect(page).to have_content("Brand Owner: C.H. Robinson Company")
+      expect(page).to have_content("Ingredients: ORGANIC SWEET POTATOES")
+      # expect(page).to have_content("")
+    end
+  end
+end
