482dce56e106d133650ab0223ee6b84749c3a72b
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-10-19 10:20:04 -0600

    Create facade and methods

---
+++ app/facade/food_facade.rb
@@ -0,0 +1,12 @@
+class FoodFacade
+  def initialize
+    @food_data = FoodService.new
+  end
+  
+  def food_results(search_string)
+    results = @food_data.search(search_string)
+    results.first[:foods].map do |data|
+      Food.new(data)
+    end
+  end
+end
